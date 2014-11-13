d950b9166fd37df04439a3cd540cbcb3baa03f8c
Alex <alextho4@hotmail.com>
2014-11-13 08:55:17 -0600

    Added login and signup
    Magical

--- AssassinsGameTracker/AssassinsGameTracker/obj/Debug/android/assassinsgametracker/assassinsgametracker/R.java
+++ AssassinsGameTracker/AssassinsGameTracker/obj/Debug/android/assassinsgametracker/assassinsgametracker/R.java
@@ -14,11 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int editText1=0x7f050001;
+        public static final int editText2=0x7f050002;
+        public static final int editText3=0x7f050004;
+        public static final int editText4=0x7f050005;
+        public static final int editText5=0x7f050003;
         public static final int myButton=0x7f050000;
+        public static final int textView1=0x7f050006;
     }
     public static final class layout {
         public static final int login=0x7f030000;
         public static final int main=0x7f030001;
+        public static final int signup=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
